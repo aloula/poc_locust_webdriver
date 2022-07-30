94a1fa12cc22e216d6d9edeaf22d11470581b39b
Alexsander Loula <alex.loula@gmail.com>
2022-07-30 00:59:14 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# PoC Locust Webdriver
+
+PoC mostrando o uso do Locust com Webdriver para simular vários usuários navegando numa página.
+
+
+### Pré-requisitos:
+
+- Python 3.8+: https://www.python.org/downloads/
+- Locust 2.10+: https://docs.locust.io/en/stable/installation.html
+- Selenium Server: https://www.seleniumhq.org/download/
+- Chromedriver: https://chromedriver.chromium.org/download/
+
+
+### Instalação (instruções Debian/Ubuntu):
+
+1 - Instale o Python 3.8+:
+```
+$ sudo apt update
+$ sudo apt upgrade
+$ sudo apt install python3 python3-venv
+```
+
+2 - Verifique a instalação:
+```
+$ python3 -V
+Python 3.8.XX
+```
+
+3 - Normalmente o Linux já vem com a versão do Python 2.7.X. Para deixar a versão 3 como padrão, acrescente a linha abaixo no ~/.bashrc:  
+alias python='python3'
+
+4 - Teste o alias:
+```
+$ source ~/.bashrc
+$ python -V
+Python 3.8.XX
+```
+
+5 - Atualize o pip, crie e ative o ambiente virtual do Python:
+```
+$ pip install --upgrade pip
+$ python -m venv .venv
+$ source .venv/bin/activate
+```
+
+5 - Instale o Locust e Selenium:
+```
+$ pip install locust
+$ pip install locust-plugins
+$ pip install selenium
+```
+
+
+### Execução:
+
+1 - Execute o Selenium Server:
+```
+$ java -jar selenium-server-4.X.X.jar standalone
+````
+
+2 - Abra outro terminal e execute o Locust:
+
+- Para execução do Locust com UI:
+```
+$ locust -f <arquivo_do_teste.py>
+```
+- Exemplo:
+```
+$ locust -f poc_locust_webdriver.py
+```
+- Acesse a interface em: <localhost:8089>
+
+
+- Para execução do Locust sem UI:
+```
+$ locust -f <arquivo_do_teste.py> --headless -u <usuários> -r <usuários/segundo> -t <tempo de teste>
+```
+- Exemplo simulando 100 usuários com subida de 10 usuários/s e 10 mins de teste:
+```
+$ locust -f poc_locust_webdriver.py --headless -u 100 -r 10 -t 10m
+```
---
+++ poc_locust_webdriver.py
@@ -0,0 +1,82 @@
+# Locust test file
+# -*- coding: utf-8 -*-
+# Uso: locust -f poc_locust_webdriver.py --headle
+# Selenium Server via Docker:
+# docker run -e SE_NODE_SESSION_TIMEOUT=60 -e SE_NODE_MAX_SESSIONS=5 -p 4444:4444 -p 7900:7900 --shm-size="2g" --rm selenium/standalone-chrome:96.0
+# Code: Loula - 2022/07/30
+
+import time
+import random
+from locust import task, constant, events, run_single_user
+from locust_plugins.users import WebdriverUser
+from locust_plugins.listeners import RescheduleTaskOnFail
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+# Configuração
+url = "https://google.com.br/"
+bands_list = ["pink floyd","led zeppelin","deep purple","beatles","queen","cream"]
+debug = True
+search_element = "/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input" #inspect
+wait_element = 5 #segundos
+timeout = 30 #segundos
+
+
+# simula usuário
+class MyUser(WebdriverUser):
+    wait_time = constant(2)
+    if __name__ == "__main__":
+        # para debug
+        if debug:
+            wait_time = constant(5)
+    else:
+        # para debug deixe False
+        if debug:
+            headless = False
+        else:
+            headless = True
+
+    def on_start(self):
+        self.client.set_window_size(1280, 720)
+        self.client.implicitly_wait(timeout)
+
+    def on_stop(self):
+        self.client.quit()
+
+    # teste
+    @task
+    def busca(self):
+        self.clear()
+        self.client.start_time = time.monotonic()  # para medir o tempo para buscar elemento da página
+        scenario_start_time = self.client.start_time  # para medir o tempo total do cenário
+        self.client.get(url)
+        self.client.add_cookie(
+            {
+                "name": "cookie_consent",
+                "value": '{"ad":true,"personalized":true,"version":0}',
+                "path": "/",
+                "secure": True,
+            }
+        )
+        self.client.find_element(By.XPATH, search_element).click() #procura campo de search
+        self.client.implicitly_wait(wait_element)
+        band = random.choice(bands_list) #seleciona um banda aleatório da lista
+        self.client.find_element(By.XPATH, search_element).send_keys(band + "\n") #digita o elemento para busca
+        self.client.implicitly_wait(wait_element)
+        self.environment.events.request.fire(
+            request_type="Busca",
+            name="Busca Produto",
+            response_time=(time.monotonic() - scenario_start_time) * 1000,
+            response_length=0,
+            exception=None,
+            context={}
+        )
+   
+@events.init.add_listener
+def on_locust_init(environment, **kwargs):
+    RescheduleTaskOnFail(environment)
+
+if __name__ == "__main__":
+    run_single_user(MyUser)
